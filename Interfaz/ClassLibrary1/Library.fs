072657db7bcdeb92dc2a79ede681952a189b41bc
Breto16 <50472204+Breto16@users.noreply.github.com>
2023-09-23 00:40:33 -0600

    Update Library.fs

--- Interfaz/ClassLibrary1/Library.fs
+++ Interfaz/ClassLibrary1/Library.fs
@@ -4,6 +4,7 @@ open System.IO
 open System.Net
 open System.Net.Sockets
 open System.Text
+open System.Net.Http
 
 module lib =
 
@@ -32,3 +33,32 @@ module lib =
             printfn "Error inesperado: %s" ex.Message
             // Puedes manejar el error aquí si es necesario
             ""
+    
+
+    let sendRequestWithFileToServer (serverIP: string) (serverPort: int) (request: string) (filePath: string) =
+        try
+            use httpClient = new HttpClient()
+        
+            // Construir un objeto MultipartFormDataContent para enviar la solicitud con un archivo adjunto
+            let content = new MultipartFormDataContent()
+            let fileContent = new ByteArrayContent(File.ReadAllBytes(filePath))
+            fileContent.Headers.ContentDisposition <- new ContentDispositionHeaderValue("form-data")
+            fileContent.Headers.ContentDisposition.Name <- "\"file\""
+            fileContent.Headers.ContentDisposition.FileName <- "\"" + Path.GetFileName(filePath) + "\""
+            content.Add(fileContent)
+
+            // Agregar otros datos de la solicitud, como el comando y los detalles de la canción
+            content.Add(new StringContent(request), "command")
+
+            // Realizar la solicitud POST al servidor Go
+            let response = httpClient.PostAsync($"http://{serverIP}:{serverPort}/endpoint", content).Result
+        
+            // Leer la respuesta del servidor Go
+            let responseBody = response.Content.ReadAsStringAsync().Result
+            responseBody
+        with
+        | ex ->
+            printfn "Error inesperado: %s" ex.Message
+            // Puedes manejar el error aquí si es necesario
+            ""
+
